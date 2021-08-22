4e1867ef3ee018d6fc2acc8e01fbcf0fdceab8dc
marunium <ishimaru0510@gmail.com>
2021-08-22 15:20:38 +0900

    プロジェクト作成

---
+++ RG02/Assets/UI/Scripts/UseCase/GetHtmlUseCase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;//UnityWebRequestを使うために必要
+
+public class GetHtmlUseCase : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		StartCoroutine(GetText());
+	}
+
+	IEnumerator GetText()
+	{
+		/*取得したいサイトURLを指定*/
+		UnityWebRequest www = UnityWebRequest.Get("https://sirohood.exp.jp/20190208-1792/");
+		yield return www.SendWebRequest();
+
+		if (www.isNetworkError || www.isHttpError)
+		{
+			Debug.Log(www.error);
+		}
+		else
+		{
+			// 結果をテキストとして表示します
+			Debug.Log(www.downloadHandler.text);
+
+			//  または、結果をバイナリデータとして取得します
+			byte[] results = www.downloadHandler.data;
+		}
+	}
+}
