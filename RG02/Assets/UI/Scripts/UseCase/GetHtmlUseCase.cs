3d2b643853b79be67691e3f34df231e382b8b3fc
marunium <ishimaru0510@gmail.com>
2021-09-12 16:17:30 +0900

    [client]　検索ボタンを押したらタグの検索をする処理追加

--- RG02/Assets/UI/Scripts/UseCase/GetHtmlUseCase.cs
+++ RG02/Assets/UI/Scripts/UseCase/GetHtmlUseCase.cs
@@ -1,34 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 using UnityEngine.Networking;//UnityWebRequestを使うために必要
+using resultHtml = System.Text.RegularExpressions.MatchCollection;
+using System;
+using System.Text;
 
 public class GetHtmlUseCase : MonoBehaviour
 {
-
-	// Use this for initialization
-	void Start()
+	public IObservable<string> GetTextAsObservable(string url)
 	{
-		StartCoroutine(GetText());
+		return Observable.FromCoroutine<string>(observer => GetTextCoroutine(observer, url));
 	}
 
-	IEnumerator GetText()
+	private IEnumerator GetTextCoroutine(IObserver<string> observer, string url)
 	{
 		/*取得したいサイトURLを指定*/
-		UnityWebRequest www = UnityWebRequest.Get("https://sirohood.exp.jp/20190208-1792/");
+		UnityWebRequest www = UnityWebRequest.Get(url);
 		yield return www.SendWebRequest();
 
 		if (www.isNetworkError || www.isHttpError)
 		{
-			Debug.Log(www.error);
+			observer.OnNext(www.error);
+			observer.OnCompleted();
 		}
 		else
 		{
 			// 結果をテキストとして表示します
-			Debug.Log(www.downloadHandler.text);
+			//Debug.Log(www.downloadHandler.text);
 
 			//  または、結果をバイナリデータとして取得します
 			byte[] results = www.downloadHandler.data;
+
+			//正規表現パターンとオプションを指定してRegexオブジェクトを作成
+			System.Text.RegularExpressions.Regex r =
+				new System.Text.RegularExpressions.Regex(
+					@"<(h[1-6])\b[^>]*>(.*?)</\1>",
+					System.Text.RegularExpressions.RegexOptions.IgnoreCase
+					| System.Text.RegularExpressions.RegexOptions.Singleline);
+
+			//TextBox1.Text内で正規表現と一致する対象をすべて検索
+			resultHtml mc = r.Matches(www.downloadHandler.text);
+
+			//foreach (System.Text.RegularExpressions.Match m in mc)
+			//{
+			//	//正規表現に一致したグループと位置を表示
+			//	Debug.Log("タグ:" + m.Groups[1].Value +
+			//		"\nタグ内の文字列:" + m.Groups[2].Value +
+			//		"\nタグの位置:" + m.Groups[1].Index);
+			//}
+
+			var stringBuilder = new StringBuilder();
+			foreach (System.Text.RegularExpressions.Match m in mc)
+			{
+				var text = $"[{ m.Groups[1].Value}]:{m.Groups[2].Value}\n";
+				stringBuilder.Append(text);
+			}
+
+			observer.OnNext(stringBuilder.ToString());
+			observer.OnCompleted();
 		}
 	}
 }
---
+++ RG02/Assets/UI/Scripts/View/SearchTagCanvasView.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UniRx;
+using UnityEngine.UI;
+using System;
+
+public class SearchTagCanvasView : MonoBehaviour
+{
+	[SerializeField] private InputField inputField;
+	[SerializeField] private Button searchButton;
+	[SerializeField] private Text resultText;
+
+	private GetHtmlUseCase getHtmlUseCase = new GetHtmlUseCase();
+
+	private void Start()
+	{
+		OnClickSearchButtonAsObservable()
+			.Do(_ => SetText(string.Empty))
+			//.SelectMany(x => getHtmlUseCase.GetTextAsObservable("https://sirohood.exp.jp/20190208-1792/"))
+			.SelectMany(x => getHtmlUseCase.GetTextAsObservable(inputField.text))
+			.Subscribe(SetText)
+			.AddTo(this);
+	}
+
+	public IObservable<string> OnEndEditAsObservable()
+	{
+		return inputField.OnEndEditAsObservable();
+	}
+
+	public IObservable<Unit> OnClickSearchButtonAsObservable()
+	{
+		return searchButton.OnClickAsObservable();
+	}
+
+	public void SetText(string text)
+	{
+		resultText.text = text;
+	}
+}
